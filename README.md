44faef1de0327d9814280f114904ef0209514e44
angysaavedrafiuba <asaavedra@fi.uba.ar>
2020-06-25 22:44:29 -0300

    Se mejora el readme y se agrega funcion crear

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # abb
 
-# Compilar con: gcc -g -std=c99 -Wall -Wconversion -Wno-sign-conversion -Werror -o pruebas *.c
-# Ejecutar con: valgrind --leak-check=full --track-origins=yes --show-reachable=yes ./pruebas
+### *Compilar con:* _gcc -g -std=c99 -Wall -Wconversion -Wno-sign-conversion -Werror -o pruebas *.c_
+### *Ejecutar con:* _valgrind --leak-check=full --track-origins=yes --show-reachable=yes ./pruebas_
--- abb.c
+++ abb.c
@@ -1,5 +1,5 @@
 #include "abb.h"
-//#include <stdlib.h>
+#include <stdlib.h>
 
 typedef struct nodo_abb{
 	struct nodo_abb* izq;
@@ -14,3 +14,17 @@ struct abb{
 	abb_destruir_dato_t destruir;
 	abb_comparar_clave_t comparar;
 };
+
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
+	abb_t* abb = malloc(sizeof(abb_t));
+
+	if (!abb) 
+		return NULL;
+
+	abb->raiz = NULL;
+	abb->cant = 0;
+	abb->destruir = destruir_dato;
+	abb->comparar = cmp;
+
+	return abb;
+}
