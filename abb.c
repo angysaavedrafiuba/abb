d87db661662e8d9e2669c469b098762afa4f6c7b
angysaavedrafiuba <asaavedra@fi.uba.ar>
2020-06-27 20:31:07 -0300

    Se cambia abb_buscar y se agregar otras primitias

--- abb.c
+++ abb.c
@@ -1,6 +1,8 @@
+#define _POSIX_C_SOURCE 200809L
 #include "abb.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct nodo_abb{
 	struct nodo_abb* izq;
@@ -11,7 +13,7 @@ typedef struct nodo_abb{
 
 struct abb{
 	nodo_abb_t* raiz;
-	size_t cant;
+	size_t cantidad;
 	abb_destruir_dato_t destruir;
 	abb_comparar_clave_t comparar;
 };
@@ -27,100 +29,98 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
 		return NULL;
 
 	abb->raiz = NULL;
-	abb->cant = 0;
+	abb->cantidad = 0;
 	abb->destruir = destruir_dato;
 	abb->comparar = cmp;
 
 	return abb;
 }
 
-// devuelve un nodo vacio e inicializado, o NULL en caso
-// de no poder reservar memoria
-nodo_abb_t* nodo_crear( void ) {
-	nodo_abb_t *nodo = malloc( sizeof(nodo_abb_t) );
-	if( !nodo ) return NULL;
-
-	nodo->clave = NULL;
-	nodo->dato = NULL;
-	nodo->der = NULL;
-	nodo->izq = NULL;
-	return nodo;
+size_t abb_cantidad(abb_t *arbol){
+	return arbol->cantidad;
 }
 
-// funcion auxiliar recursiva para abb_buscar
-nodo_abb_t* abb_buscar_rec( nodo_abb_t *raiz, const char *clave, abb_comparar_clave_t comparar ) {
-	if( (raiz->der == NULL) && (raiz->izq == NULL) ) {
-		return raiz;
-	}
-
+nodo_abb_t* abb_buscar( nodo_abb_t *raiz, const char *clave, abb_comparar_clave_t comparar, nodo_abb_t** padre) {
 	int comparacion = comparar( clave, raiz->clave );
-	nodo_abb_t * nodo_resultado;
+	nodo_abb_t* nodo_resultado = NULL;
+
 	if( comparacion == 0 ) {
 		nodo_resultado = raiz;
 	}
 	else if( (comparacion > 0) && (raiz->der != NULL) ) {
-		nodo_resultado = abb_buscar_rec( raiz->der, clave, comparar );
+		nodo_resultado = abb_buscar( raiz->der, clave, comparar, &raiz );
 	}
 	else if( (comparacion < 0) && (raiz->izq != NULL) ) {
-		nodo_resultado = abb_buscar_rec( raiz->izq, clave, comparar );
+		nodo_resultado = abb_buscar( raiz->izq, clave, comparar, &raiz );
 	}
 	return nodo_resultado;
-}
-
-// devuelve el nodo donde se encuentra el par (clave, dato).
-// En caso de que no esté en el ABB, se devuelve el nodo 
-// que lo tendria como hijo si existiera.
-// pre: se inicializó el abb y tiene al menos 1 elemento 
-nodo_abb_t* abb_buscar( const abb_t *abb, const char *clave ) {
-	nodo_abb_t *nodo = abb_buscar_rec( abb->raiz, clave, abb->comparar );
-	return nodo;
-}
-
-size_t abb_cantidad( abb_t *arbol ) {
-	return arbol->cant;
 }
 
 bool abb_guardar( abb_t *arbol, const char *clave, void *dato ) {
 	nodo_abb_t *nodo; //nodo donde guardaremos el dato
 	if( abb_cantidad(arbol) == 0 ) {
-		nodo = nodo_crear();
+		nodo = calloc(1, sizeof(nodo_abb_t));
 		if( !nodo ) return false;
 		arbol->raiz = nodo;
-		arbol->cant ++;
+		arbol->cantidad++;
 	}
 	else {
-		nodo_abb_t *nodo_aux = abb_buscar( arbol, clave );
-		int comp = arbol->comparar( clave, nodo_aux->clave );
-		if( comp == 0 ) { //hay que reemplazar
+		nodo_abb_t* padre = arbol->raiz;
+		nodo_abb_t *nodo_aux = abb_buscar(arbol->raiz, clave, arbol->comparar, &padre);
+		if(nodo_aux) {
 			free( nodo_aux->clave );
 			arbol->destruir( nodo_aux->dato );
 			nodo = nodo_aux;
 		}
-		else if( comp > 0 ) {
-			nodo_aux->der = nodo_crear();
-			if( !nodo_aux->der ) return false;
-			nodo = nodo_aux->der;
-			arbol->cant ++;
-		}
-		else if( comp < 0 ) {
-			nodo_aux->izq = nodo_crear();
-			if( !nodo_aux->izq ) return false;
-			nodo = nodo_aux->izq;
-			arbol->cant ++;
-		}
+		else{
+			nodo = calloc(1, sizeof(nodo_abb_t));
+			if( !nodo ) return false;
+
+			int comp = arbol->comparar(clave, padre->clave);
+
+			if( comp > 0 )
+				padre->der = nodo;
+			else if( comp < 0 )
+				padre->izq = nodo;
+
+			arbol->cantidad++;
+		} 
 	}
 	nodo->clave = strdup(clave);
 	nodo->dato = dato;
 	return true;
 }
 
-void *abb_obtener( const abb_t *arbol, const char *clave ) {
-	if( arbol->cant == 0 )
-		return NULL;
-	
-	nodo_abb_t *nodo = abb_buscar( arbol, clave );
-	if( strcmp( clave, nodo->clave ) == 0 )
-		return nodo->clave;
+void *abb_obtener(const abb_t *arbol, const char *clave){
+	nodo_abb_t* nodo = abb_buscar(arbol->raiz, clave, arbol->comparar, NULL);
 
-	return NULL;
+	if (!nodo)
+		return NULL;
+
+	return nodo->dato;
 }
+
+bool abb_pertenece(const abb_t *arbol, const char *clave){
+	if (!abb_buscar(arbol->raiz, clave, arbol->comparar, NULL))
+		return false;
+
+	return true; 
+}
+
+void abb_destruir_rec(nodo_abb_t* raiz, abb_destruir_dato_t destruir){
+	if(!raiz) return;
+
+	abb_destruir_rec(raiz->izq, destruir);
+	abb_destruir_rec(raiz->der, destruir);
+
+	free(raiz->clave);
+	destruir(raiz->dato);
+	free(raiz);
+}
+
+void abb_destruir(abb_t *arbol){
+	if (arbol->cantidad > 0)
+		abb_destruir_rec(arbol->raiz, arbol->destruir);
+	free(arbol);
+}
+
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -3,7 +3,81 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void abb_destruir_aux(void *abb) { abb_destruir(abb); }
+
+void prueba_crear_abb_destruir_free() {
+  printf("------Inicio pruebas crear abb con free\n");
+  abb_t *ejemplo = abb_crear(strcmp,free);
+  print_test("abb creado", ejemplo != NULL);
+  abb_destruir(ejemplo);
+}
+
+void prueba_crear_abb_destruir_funcion() {
+  printf("------Inicio pruebas crear abb con funcion destruir\n");
+  abb_t *ejemplo = abb_crear(strcmp,abb_destruir_aux);
+  print_test("abb creado", ejemplo != NULL);
+  abb_destruir(ejemplo);
+}
+
+void prueba_destruir_abb_vacio_con_free() {
+  printf("------Inicio pruebas destruir abb vacio con funcion destruir free\n");
+  abb_t *ejemplo = abb_crear(strcmp, free);
+  print_test("Abb tiene cantidad 0", !abb_cantidad(ejemplo));
+  abb_destruir(ejemplo);
+  print_test("abb destruido", true);
+}
+
+void prueba_destruir_abb_vacio_con_funcion() {
+  printf("------Inicio pruebas destruir abb vacio con funcion destruir funcion\n");
+  abb_t *ejemplo = abb_crear(strcmp, abb_destruir_aux);
+  print_test("Abb tiene cantidad 0", !abb_cantidad(ejemplo));
+  abb_destruir(ejemplo);
+  print_test("abb destruido", true);
+}
+
+void prueba_destruir_abb_no_vacio_con_free() {
+  printf("------Inicio pruebas destruir abb no vacio con parámetro free\n");
+  abb_t *ejemplo = abb_crear(strcmp, free);
+  void *valores[10];
+  char str[2];
+  
+  for (int i = 0; i < 10; i++) {
+    valores[i] = malloc(sizeof(char));
+    sprintf(str, "%d", i);
+    abb_guardar(ejemplo, str, valores[i]);
+  }
+
+  printf("Cantidad %d\n", (int)abb_cantidad(ejemplo));
+  print_test("abb después de insertar valores NO está vacio", abb_cantidad(ejemplo));
+  abb_destruir(ejemplo);
+  print_test("abb destruido", true);
+}
+/*
+void prueba_destruir_abb_no_vacia_con_funcion() {
+  printf(
+      "------Inicio pruebas destruir abb no vacía con parámetro función\n");
+  abb_t *ejemplo = abb_crear();
+  void *valores[10];
+
+  for (int i = 0; i < 10; i++) {
+    valores[i] = abb_crear();
+    abb_insertar_primero(ejemplo, valores[i]);
+  }
+
+  print_test("abb después de insertar valores NO está vacia",
+             !abb_esta_vacia(ejemplo));
+  abb_destruir(ejemplo, abb_destruir_aux);
+  print_test("abb destruida con funcion", true);
+}
+*/
 void pruebas_abb_alumno() {
+
+	prueba_crear_abb_destruir_free();
+	prueba_crear_abb_destruir_funcion();
+	prueba_destruir_abb_vacio_con_free();
+	prueba_destruir_abb_vacio_con_funcion();
+	prueba_destruir_abb_no_vacio_con_free();
 
 }
